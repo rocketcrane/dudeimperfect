d48b908456cb454c1cd104f491f7ce17bd3eb4c5
jlee@vassar.edu <jlee@dhcp-pool-240.cs.vassar.edu>
2020-04-07 12:59:54 -0400

    close with finsihing the equations

---
+++ BrianTS.js
@@ -0,0 +1,66 @@
+/*
+ * Brian Lee 04/07/2020 
+ * Trickshot heaven JS code / projectile motion
+ */
+
+//game variable
+let x;
+let y;
+
+//user inupt
+let a; // launch angle in degrees
+let f; // force in N
+
+//calculation
+let m = 1; // a basketball weighs 1 kg 
+let tfa = 1; // time of force applied is 1 s
+let v; // initial projectile velocity. f = ma
+let r; // launch angle in radians
+let vx, vy; // initial horizontal velocity = cos0 * v, vertical velocity = sin0 * v
+let tx, ty1, ty2; // times to reach target based on horizontal & vertical velocities
+let g = 9.8; // gravity acceleation: 9.8 m/s/s
+
+// paramters & return values all in units meters & seconds
+// xdis = xvel * xtime --> xtime = xdis/xvel
+function xt(xd,xv) {
+  return xd/xv;
+}
+// ydis = yvel*ytime - g*ytime*ytime/2 
+// --> g*ytime*ytime/2 + yvel*ytime - ydis = 0
+// --> ytime = [-yvel ± √(yvel*yvel - 4*(g/2)*(-ydis)]/(2*(g/2))
+function yt1(yd,yv) {
+  let eval = [-yv + Math.sqrt(yv*yv + 4*(g/2)*yd)]/(2*(g/2));
+  return eval;
+}
+function yt2(yd,yv) {
+  let eval = [-yv - Math.sqrt(yv*yv + 4*(g/2)*yd)]/(2*(g/2));
+  return eval;
+}
+
+function hit(xt, yt1, yt2){
+  if((xt == yt1)||(xt == yt2)) return 1;
+  else return 0;
+}
+
+// animation
+function yvel(vv,t) return vv - g*t;
+
+function setup() {
+  x = 10;
+  y = 5;
+  a = 60;
+  f = 10;
+  v = f*tfa/m; // initial projectile velocity. f = ma
+  r = a/360*2*PI; // launch angle in radians
+  vx = Math.cos(r)*v; // initial horizontal velocity = cos0 * v
+  vy = Math.sin(r)*v; // initial vertical velocity = sin0 * v
+  tx = xt(x,vx);
+  ty1 = yt1(y,vy);
+  ty2 = yt2(y,vy);
+}
+
+
+function draw() {
+  if(hit(tx, ty1, ty2) == 1)
+  else
+}
