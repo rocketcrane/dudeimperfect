fb4e199f27958b21aba2ed45117aef53db237f86
jlee@vassar.edu <jlee@dhcp-pool-240.cs.vassar.edu>
2020-04-07 14:53:21 -0400

    still lack animation but code is more complete

--- BrianTS.js
+++ BrianTS.js
@@ -6,6 +6,10 @@
 //game variable
 let x;
 let y;
+let ORIGIN = [50, 380];
+let scaleToScreen = 10;
+let count = 0;
+let bucket = 1;
 
 //user inupt
 let a; // launch angle in degrees
@@ -18,15 +22,15 @@ let v; // initial projectile velocity. f = ma
 let r; // launch angle in radians
 let vx, vy; // initial horizontal velocity = cos0 * v, vertical velocity = sin0 * v
 let tx, ty1, ty2; // times to reach target based on horizontal & vertical velocities
-let g = 9.8; // gravity acceleation: 9.8 m/s/s
+let g = -9.8; // gravity acceleation: -9.8 m/s/s. Negative because its going down
 
 // paramters & return values all in units meters & seconds
 // xdis = xvel * xtime --> xtime = xdis/xvel
 function xt(xd,xv) {
   return xd/xv;
 }
-// ydis = yvel*ytime - g*ytime*ytime/2 
-// --> g*ytime*ytime/2 + yvel*ytime - ydis = 0
+// ydis = yvel*ytime + g*ytime*ytime/2 
+// --> g/2*ytime*ytime + yvel*ytime - ydis = 0
 // --> ytime = [-yvel ± √(yvel*yvel - 4*(g/2)*(-ydis)]/(2*(g/2))
 function yt1(yd,yv) {
   let eval = [-yv + Math.sqrt(yv*yv + 4*(g/2)*yd)]/(2*(g/2));
@@ -38,8 +42,10 @@ function yt2(yd,yv) {
 }
 
 function hit(xt, yt1, yt2){
-  if((xt == yt1)||(xt == yt2)) return 1;
-  else return 0;
+  if((xt == yt1)||(xt == yt2)) {
+    bucket = xt;
+    return 1;
+  }else return 0;
 }
 
 // animation
@@ -47,36 +53,44 @@ function yvel(vv,t) {
   return vv - g*t;
 }
 
+
 function setup() {
-  x = 10;
-  y = 5;
+  x = 24;
+  y = 12.4;
   a = 60;
-  f = 10;
-  v = f*tfa/m; // initial projectile velocity. f = ma
-  r = a/360*2*PI; // launch angle in radians
-  vx = Math.cos(r)*v; // initial horizontal velocity = cos0 * v
-  vy = Math.sin(r)*v; // initial vertical velocity = sin0 * v
-  tx = xt(x,vx);
-  ty1 = yt1(y,vy);
-  ty2 = yt2(y,vy);
+  f = 20;
+  v = f*tfa/m; // initial projectile velocity = 20 m/s. f = ma
+  r = a/360*2*PI; // launch angle in radians = PI/3
+  vx = Math.cos(r)*v; // initial horizontal velocity, cos0 * v = 12
+  vy = Math.sin(r)*v; // initial vertical velocity, sin0 * v = 16
+  tx = xt(x,vx); // 24/12 = 2 s
+  ty1 = yt1(y,vy); // [-16 + sqrt(16*16 - 4(1/2)(-9.8)(-12.4))]/(2(1/2)(-9.8)) = 1.27 s
+  ty2 = yt2(y,vy); // [-16 - sqrt(16*16 - 4(1/2)(-9.8)(-12.4))]/(2(1/2)(-9.8)) = 2 s
+  
   createCanvas(500, 500); //size of program window on browswer
   background(0);
   frameRate(30);
+  
   textAlign(CENTER,TOP);
   fill(255);
-  circle(50, 380, 20);
-  triangle(40,400,50,390,60,400);
-  rect(400,250,40,3);
-}
-
-
-function draw() {
   textSize(15);
   text("The hoop is "+str(x)+" meters away and "+str(y)+" meters high", width/2, 20);
   textSize(20);
   text("Launch Angle: "+str(a)+"°", width/2, 50); 
   text("Launch Force: "+str(f)+" N", width/2, 80);
   
-  //if(hit(tx, ty1, ty2) == 1)
-  //else
+  triangle(40,400,50,390,60,400);
+  rect((ORIGIN[0]+x*scaleToScreen),(ORIGIN[1]-y*scaleToScreen),40,3);
+}
+
+
+function draw() {
+  circle(ORIGIN[0], ORIGIN[1], 20);
+  if(count == bucket){
+    if(hit(tx, ty1, ty2) == 1){
+      background('rgb(0,255,0)');
+    }else{
+      background('red');
+    }
+  }
 }
