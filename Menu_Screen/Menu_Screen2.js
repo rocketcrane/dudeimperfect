ce8953a262dc9cf8e3083cc14473a1cf7f1898c6
maverin-vassar <47159259+maverin-vassar@users.noreply.github.com>
2020-04-20 19:34:44 -0400

    Sprint 3 Main Menu

---
+++ Menu_Screen/Menu_Screen2.js
@@ -0,0 +1,21 @@
+function playState(){
+  screenState = 1;
+}
+
+function helpState(){
+  screenState = 2;
+}
+
+function tutorialState(){
+  screenState = 3;
+}
+
+function hideButtons(){
+  playButton.hide();
+  helpButton.hide();
+}
+
+function showButtons(){
+  playButton.show();
+  helpButton.show();
+}
