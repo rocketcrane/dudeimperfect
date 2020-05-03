9f7daa4c93d5b6f6c38668dca127956b1894ee0c
Brian Lee <the930325@yahoo.com>
2020-05-03 15:09:22 -0400

    updated background. working

---
+++ Sprint4/AAA_Game.js
@@ -0,0 +1,7 @@
+class Game {
+  constructor(grav, time_force_applied) {
+    // distance/height of the hoop with respect to the ball at the launching point
+    this.grav = grav;
+    this.time_force_applied = time_force_applied;
+  }
+}
